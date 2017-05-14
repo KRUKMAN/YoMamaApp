dfefd42157132a689b1735e9a7e2bbb8ade10866
Jakub Krukowski <jakubkrukowski@jakubs-imac.home>
2017-05-15 00:26:53 +0200

    Add random number generator and configure view

--- YoMamaApp/ViewController.swift
+++ YoMamaApp/ViewController.swift
@@ -9,28 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var diss = ["is so fat she eats shit", "suck dick", "is a really nice woman"]
+    
+    @IBOutlet weak var yoMamaLabel: UILabel!
+    @IBOutlet weak var yoMamaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red:1.00, green:0.15, blue:0.05, alpha:1.0)
-
+        configureView()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dissButtonPushed(_ sender: UIButton) {
+        let number = Int(arc4random_uniform(UInt32(diss.count)))
+        yoMamaLabel.isHidden = false
+        yoMamaLabel.text = "Yo mama \(diss[number])"
     }
-    */
-
+    
+        func configureView() {
+        yoMamaButton.layer.cornerRadius = 5
+        yoMamaButton.backgroundColor = UIColor.white
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
